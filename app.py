bf3a8cb2c861e963834bf1a30c94ff395b2a74f2
Gurpreetwork9582 <Gurpreetsingh9582.work@gmail.com>
2026-03-19 22:18:53 -0300

    changes 6

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
 def contact():
     return render_template("contact.html")
 
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
